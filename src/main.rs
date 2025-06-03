ab1315497e0cd14ffdefcc1b5af4ac666699c6b3
Suleimen Zhonissov <zhonisov.s@gmail.com>
2025-06-03 11:31:14 +0500

    testing game of life

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use bevy::{DefaultPlugins, app::App};
 
 use cellular_automata::CellularAutomataPlugin;
+use game_of_life::GameOfLifePlugin;
 
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
-    .add_plugins(CellularAutomataPlugin(10))
+    .add_plugins(GameOfLifePlugin)
     .run();
 }
